1d67c4eb0c2e86af712c3c27ad4f1191613bd755
Lachlan Miller <lachlan.miller.1990@outlook.com>
2019-05-10 22:05:23 +1000

    Add login/logout methods

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email].downcase)
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id.to_s
+      redirect_to posts_path, notice: 'Logged in.'
+    else
+      flash.now.alert = 'Incorrect email/password combination.'
+      render :new
+    end
+  end
+
+  def destroy
+    session.delete :user_id
+    redirect_to login_path, notice: 'Logged out.'
+  end
+
+  private
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get 'users/new' => 'users#new', as: :new_user
   post 'users' => 'users#create'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
   resources :posts do
     scope module: 'posts' do
       resources :corrections
